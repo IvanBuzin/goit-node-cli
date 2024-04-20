f40d0b1d0b747646bc6dd4d4a303898e8ffd0c6b
IvanBuzin <ua.buzin@gmail.com>
2024-04-20 15:56:29 +0300

    er

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { program } = require("commander");
 const contacts = require("./contacts");
 
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option("-a, --action, <type>")
+  .option("-i, --id, <type>")
+  .option("-n, --name, <type>")
+  .option("-e, --email, <type>")
+  .option("-p, --phone, <type>");
 
 program.parse();
 
